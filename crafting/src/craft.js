fd36c83ee3ce1f031a2499ab4b473b1be6687f81
Eli Sachs <easachs13@gmail.com>
2022-12-09 01:22:26 -0700

    Feat: add material/craft

--- crafting/src/craft.js
+++ crafting/src/craft.js
@@ -1,5 +1,24 @@
-class Craft {
+var Material = require('./material');
 
+class Craft {
+  constructor(attributes) {
+    this.name = attributes.type;
+    this.materials = attributes.materials;
+    this.completed = false;
+  }
+
+  completeCraft() {
+    this.completed = true;
+    return 'All done! It looks great!';
+  }
+
+  calculateProjectTotal() {
+    var total = 0;
+    for (var i = 0; i < this.materials.length; i++) {
+      total += this.materials[i].calculateMaterialCost();
+    }
+    return total
+  }
 }
 
 module.exports = Craft;
